034f5fac680c625944a33b626c5e4c260bf3ab53
Eero Vilpponen <eero.vilpponen@gmail.com>
2026-04-15 16:07:25 +0300

    Maybe fix photos

--- photos/index.js
+++ photos/index.js
@@ -1,85 +1,82 @@
-console.log("Hello world!");
-
-var GoogleAuth;
+// Google Photos Library API + Google Identity Services (OAuth 2.0 token model).
+// Replaces deprecated gapi.auth2 / Google Sign-In for Web.
+var CLIENT_ID =
+  "734651298434-6lukaeulk76bjmj4pkv456lt1bbd1f8v.apps.googleusercontent.com";
 var SCOPE = "https://www.googleapis.com/auth/photoslibrary.readonly";
-function handleClientLoad() {
-  // Load the API's client and auth2 modules.
-  // Call the initClient function after the modules load.
-  gapi.load("client:auth2", initClient);
-}
+var tokenClient;
 
 function initClient() {
-  // Retrieve the discovery document for version 3 of Google Drive API.
-  // In practice, your app can retrieve one or more discovery documents.
-  var discoveryUrl =
-    "https://photoslibrary.googleapis.com/$discovery/rest?version=v1";
-
-  // Initialize the gapi.client object, which app uses to make API requests.
-  // Get API key and client ID from API Console.
-  // 'scope' field specifies space-delimited list of access scopes.
+  // Do not load discoveryDocs: fetching the Photos Library discovery document
+  // fails with HTTP 400 from current gapi (bad pp/fields query). All requests
+  // below use full REST paths, so discovery is unnecessary.
   gapi.client
-    .init({
-      apiKey: "AIzaSyBLgTeDjqm0X3800xEQzxo9bCa3HYIfpns",
-      discoveryDocs: [discoveryUrl],
-      clientId: "734651298434-6lukaeulk76bjmj4pkv456lt1bbd1f8v.apps.googleusercontent.com",
-      scope: SCOPE
-    })
-    .then(function() {
-      GoogleAuth = gapi.auth2.getAuthInstance();
+    .init({})
+    .then(function () {
+      tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPE,
+        callback: function (tokenResponse) {
+          if (tokenResponse && tokenResponse.error) {
+            console.error(tokenResponse);
+            return;
+          }
+          if (tokenResponse && tokenResponse.access_token) {
+            gapi.client.setToken(tokenResponse);
+            setSigninStatus();
+          }
+        },
+      });
 
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-      // Handle initial sign-in state. (Determine if user is already signed in.)
-      var user = GoogleAuth.currentUser.get();
-      setSigninStatus();
-
-      // Call handleAuthClick function when user clicks on
-      //      "Sign In/Authorize" button.
-      $("#sign-in-or-out-button").click(function() {
+      $("#sign-in-or-out-button").click(function () {
         handleAuthClick();
       });
-      $("#revoke-access-button").click(function() {
+      $("#revoke-access-button").click(function () {
         revokeAccess();
       });
+
+      setSigninStatus();
+    })
+    .catch(function (err) {
+      console.error("gapi.client.init failed", err);
     });
 }
 
 function handleAuthClick() {
-  if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked 'Sign out' button.
-    GoogleAuth.signOut();
+  var token = gapi.client.getToken();
+  if (token && token.access_token) {
+    gapi.client.setToken("");
+    setSigninStatus();
   } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
+    tokenClient.requestAccessToken({ prompt: "" });
   }
 }
 
 function revokeAccess() {
-  GoogleAuth.disconnect();
-}
-
-function setSigninStatus(isSignedIn) {
-  var user = GoogleAuth.currentUser.get();
-  var isAuthorized = user.hasGrantedScopes(SCOPE);
-  if (isAuthorized) {
-    $("#sign-in-or-out-button").css("display", "none")//.html("Sign out");
-    $("#revoke-access-button").css("display", "none");//"inline-block");
-    $("#auth-status").html('');
-    //   "You are currently signed in and have granted " + "access to this app."
-    // );
-    listAlbums();
-  } else {
-    $("#sign-in-or-out-button").html("Sign In/Authorize");
-    $("#revoke-access-button").css("display", "none");
-    $("#auth-status").html(
-      "You have not authorized this app or you are " + "signed out."
-    );
+  var token = gapi.client.getToken();
+  if (token && token.access_token) {
+    google.accounts.oauth2.revoke(token.access_token, function () {
+      gapi.client.setToken("");
+      setSigninStatus();
+    });
   }
 }
 
-function updateSigninStatus(isSignedIn) {
-  setSigninStatus();
+function setSigninStatus() {
+  var token = gapi.client.getToken();
+  var isAuthorized = token && token.access_token;
+  if (isAuthorized) {
+    $("#sign-in-or-out-button").css("display", "none");
+    $("#revoke-access-button").css("display", "none");
+    $("#auth-status").html("");
+    listAlbums();
+  } else {
+    $("#sign-in-or-out-button").css("display", "inline-block");
+    $("#sign-in-or-out-button").html("Sign In/Authorize");
+    $("#revoke-access-button").css("display", "none");
+    $("#auth-status").html(
+      "You have not authorized this app or you are signed out."
+    );
+  }
 }
 
 var currentMediaItem = null;
@@ -437,3 +434,5 @@ window.addEventListener('load', function() {
         }
     })
 })
+
+gapi.load("client", initClient);
